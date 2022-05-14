4c5b9639fb0042df58b51d9ac2661edd703b27dd
Piyushroy1 <piyushroychowdhury5@gmail.com>
2022-05-14 22:16:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Dropdown from 'react-bootstrap/Dropdown'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      
+      <Form className='Predict-form'>
+
+        <img className='mb-4 upes-Logo'
+          src='https://indiaeducationdiary.in/wp-content/uploads/2022/01/UPES-LOGO-01.jpg'></img>
+          
+        <Form.Group className="mb-3" controlId="formProductName">
+        <Form.Label>Name of the product:</Form.Label>
+          <input className='ProdName' type="text" placeholder="Enter the product name"/>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formCategory">
+        <Dropdown>
+  <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+    Category
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">A</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">B</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">C</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formType">
+        <Dropdown>
+  <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+    Type
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    <Dropdown.Item href="#/action-1">A</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">B</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">C</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+        </Form.Group>
+
+
+        <Form.Group className="mb-3" controlId="formSpecifictaions">
+        <Form.Label>Specifications:</Form.Label>
+          <input className='ProdSpecifications' type="text" placeholder='Specifications'/>
+          <input className='ProdSpecifications2' type="text" placeholder='Specifications'/>
+          <input className='ProdSpecifications2' type="text" placeholder='Specifications'/>
+          <input className='ProdSpecifications2' type="text" placeholder='Specifications'/>
+        </Form.Group>
+
+
+        <Button variant="success" type="submit">
+    Predict
+  </Button>
+
+      </Form>
+
+    </Container>
   );
 }
 

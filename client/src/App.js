892504dfc330464e7b58a4e01d337c2211e1a8a7
Piyushroy1 <piyushroychowdhury5@gmail.com>
2022-05-15 16:28:42 +0530

    new commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Dropdown from 'react-bootstrap/Dropdown'
 import Button from 'react-bootstrap/Button'
 import './App.css';
 
